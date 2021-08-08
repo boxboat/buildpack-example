aa90ec920539095b293204f6cc81eee22ed55f7f
dsmorrison <dsm0014@tigermail.auburn.edu>
2021-08-08 12:49:43 -0500

    use built-in project.toml configuration

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/usr/local/bin/bash
+
+
+build_and_push() {
+    img=$(grep '^id\s*=' ./project.toml | sed 's/^.*"\([^"]*\)".*$/\1/')
+
+    pack build "k3d-myregistry.localhost:12345/$img:local" --builder=gcr.io/buildpacks/builder:v1  --path=.
+    docker push "k3d-myregistry.localhost:12345/$img:local"
+}
+
+
+# $1 Array of relative paths to project sources
+micro_svc_builder() {
+  web_service_sources=($@)
+  base_dir=$(pwd)
+
+  echo "---> Starting Buildpack proccess"
+  for src in "${web_service_sources[@]}"; do
+
+    full_src_dir="$base_dir/$src"
+    cd "$src" || exit
+
+    if [[ ! -f ./project.toml ]]; then
+      echo "!--> ERROR: Did not find 'project.toml' file in source directory $full_src_dir"
+      exit 100
+    fi
+
+    echo "---> Performing setup for project directory $full_src_dir"
+    build_and_push
+
+    cd "$base_dir" || exit
+
+  done
+}
+
+
+micro_svc_builder "$@"
